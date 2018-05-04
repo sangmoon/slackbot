27a700c8265da5a2fe78e3d904d279e0d0bdfa7b
sangmoon <cholsoo22001@gmail.com>
2018-05-04 11:27:07 +0900

    KeyError 캐치 코드 추가

--- slack_sync.py
+++ slack_sync.py
@@ -21,7 +21,7 @@ try:
                     ws.send(json.dumps({
                         'channel': response['channel'],
                         'type': 'message',
-                        'text': '`!명령어`/`!정보`/`!봇`/`SAS랜덤`',
+                        'text': '`!명령어`/`!정보`/`!봇`/`!SAS랜덤`',
                     }))
                 elif "준연" in response['text'] or "갓" in response['text']:
                     ws.send(json.dumps({
@@ -34,7 +34,13 @@ try:
                     ws.send(json.dumps({
                         'channel': response['channel'],
                         'type': 'message',
-                        'text': '''레드마인 : http://192.168.2.185:2222/ \n 소프트 세미나: http://192.168.13.80/mediawiki/index.php/Main_Page  \n 클라우드실 교육자료 : http://192.168.2.185:2222/projects/temp/wiki/%EC%97%B0%EA%B5%AC%EC%86%8C_%EA%B3%B5%ED%86%B5_%EA%B5%90%EC%9C%A1_%EB%B0%8F_%EC%BB%A4%EB%A6%AC%ED%81%98%EB%9F%BC \n PAS실 자료 http://192.168.2.175:10080/cloud3/ProObjectPlus/wikis/home''',
+                        'text': '''
+                                    레드마인 : http://192.168.2.185:2222/ \n
+                                    소프트 PM팀: http://192.168.13.80/mediawiki/index.php/Main_Page  \n
+                                    클라우드실 교육자료 : http://192.168.2.185:2222/projects/temp/wiki/%EC%97%B0%EA%B5%AC%EC%86%8C_%EA%B3%B5%ED%86%B5_%EA%B5%90%EC%9C%A1_%EB%B0%8F_%EC%BB%A4%EB%A6%AC%ED%81%98%EB%9F%BC \n
+                                    PAS실 자료 http://192.168.2.175:10080/cloud3/ProObjectPlus/wikis/home \n
+                                    PO manager 서버: 192.168.15.70:8080
+                                ''',
                     }))
                 elif "!봇" == response['text']:
                     ws.send(json.dumps({
@@ -45,7 +51,7 @@ try:
                                 ''',
                     }))
 
-                elif "SAS랜덤" in response['text']:
+                elif "!SAS랜덤" in response['text']:
                     lst = ['상문', '준연', '성수', '종호', '희수', '상준',
                            '도은', '지원', '민희', '유나']
                     num_of_select = 2
@@ -69,6 +75,8 @@ try:
                     pass
 
 except KeyboardInterrupt:
+    pass
+except KeyError:
     pass
 finally:
     ws.close()
